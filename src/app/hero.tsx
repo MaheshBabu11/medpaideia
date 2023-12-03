911cb21fa3cac7970c954229e47d24ad704e9ea7
MaheshBabu11 <mahesh.b.pec@gmail.com>
2023-12-03 19:47:31 +0530

    commiting edits as requested

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -3,78 +3,65 @@
 import Image from "next/image";
 import { Button, Typography } from "@material-tailwind/react";
 
-
 function Hero() {
   return (
     <div className="relative min-h-screen w-full">
-      <header className="grid !min-h-[49rem] bg-gray-900 px-8">
+      <header
+        className="grid !min-h-[49rem] bg-gray-900 px-8 bg-center bg-cover px-8 "
+        style={{
+          backgroundImage:
+            "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('/image/bg.jpg')",
+        }}
+      >
+        {/* <div className="absolute inset-0 bg-black opacity-20"></div> */}
         <div className="container mx-auto mt-32 grid h-full w-full grid-cols-1 place-items-center lg:mt-14 lg:grid-cols-2">
           <div className="col-span-1">
-            <Typography variant="h1" color="white" className="mb-4">
-              Your One Stop <br /> Medical Education App
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4  backdrop-blur-sm "
+            >
+              Your One Stop <br /> Medical Education Platform
             </Typography>
             <Typography
               variant="lead"
-              className="mb-7 !text-white md:pr-16 xl:pr-28"
+              className="mb-7 !text-white md:pr-16 xl:pr-28 backdrop-blur-sm"
             >
-              Our app is here to empower you on your quest for knowledge,
-              anytime and anywhere.
+              The ultimate companion for medical students designed with
+              cutting-edge technology and curated by experts in the field, We
+              are re-defining medical education by offering a comprehensive
+              solution tailored to your learning needs.
             </Typography>
-            <Typography className="mb-4" color="white" variant="h6">
-              Get the app
-            </Typography>
-            <div className="flex flex-col gap-2 md:mb-2 md:w-10/12 md:flex-row">
-              <Button
-                size="lg"
-                color="white"
-                className="flex justify-center items-center gap-3"
-              >
-                <Image
-                  width={256}
-                  height={256}
-                  src="/logos/logo-apple.png"
-                  alt="metamask"
-                  className="w-6 h-6"
-                />
-                App Store
-              </Button>
-              <Button
-                size="lg"
-                color="white"
-                className="flex justify-center items-center gap-3"
-              >
-                <Image
-                  width={256}
-                  height={256}
-                  src="/logos/logo-google.png"
-                  alt="metamask"
-                  className="w-6 h-6"
-                />
-                Google Play
-              </Button>
-            </div>
           </div>
-          <Image
+          {/* <Image
             width={470}
             height={576}
             src="/image/iphones.png"
             alt="team work"
             className="col-span-1 my-20 h-full max-h-[30rem] -translate-y-32 md:max-h-[36rem] lg:my-0 lg:ml-auto lg:max-h-[40rem] lg:translate-y-0"
-          />
+          /> */}
         </div>
       </header>
       <div className="mx-8 lg:mx-16 -mt-24 rounded-xl bg-white p-5 md:p-14 shadow-md">
         <div>
           <Typography variant="h3" color="blue-gray" className="mb-3">
-            Learning App
+            Our Mission
           </Typography>
           <Typography
             variant="paragraph"
-            className="font-normal !text-gray-500 lg:w-5/12"
+            className="font-normal !text-gray-500"
           >
-            Download our app to dive into a vast library of courses, tutorials,
-            and study materials on a wide range of subjects - from programming
-            and language learning to personal development and beyond
+            <strong>Medpedia</strong> stands at the forefront of revolutionizing medical
+            education. Seamlessly blending technological innovation and curated
+            expertise, we provide a transformative learning environment tailored
+            for aspiring medical professionals. Our platform transcends
+            traditional boundaries, offering interactive and engaging
+            experiences that empower learners to grasp intricate medical
+            concepts with ease. Through our dedication to cutting-edge
+            technology and the guidance of seasoned professionals, Medpedia
+            creates an immersive academic journey. We empower students to
+            navigate the dynamic landscape of healthcare with confidence,
+            equipping them with the skills and knowledge essential for success.
           </Typography>
         </div>
       </div>
