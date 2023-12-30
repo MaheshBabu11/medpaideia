414d0a634734474d44ebeb5c14d3672f6902e7d5
MaheshBabu11 <mahesh.b.pec@gmail.com>
2023-12-30 21:58:01 +0530

    refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import VideoIntro from "./video-intro";
 import Feature from "./feature";
 import MobileConvenience from "./mobile-convenience";
 import Testimonials from "./testimonials";
-import Faqs from "./faqs";
+//import Faqs from "./faqs";
 
 export default function Campaign() {
   return (
