33b7e0e293b1d9f46670ee2c402b57df06f338b5
MaheshBabu11 <mahesh.b.pec@gmail.com>
2024-01-17 12:40:47 +0530

    removing pages

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ export * from "./layout";
 export * from "./page";
 export * from "./video-intro";
 export * from "./feature";
-export * from "./faqs";
+// export * from "./faqs";
 export * from "./mobile-convenience";
 export * from "./testimonials";
 
