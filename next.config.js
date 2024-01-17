87fc4f616dd5df3d9092feb83bf2e6cd67b5f911
MaheshBabu11 <mahesh.b.pec@gmail.com>
2024-01-17 21:00:27 +0530

    pwa update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -10,4 +13,11 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+// Configuration object tells the next-pwa plugin
+const withPWAConfig = withPWA({
+  dest: 'public', // Destination directory for the PWA files
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+module.exports = withPWAConfig(nextConfig);
