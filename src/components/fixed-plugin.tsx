4473f1d800ca246008639bd9354882dea2d7eeee
MaheshBabu11 <mahesh.b.pec@gmail.com>
2024-01-17 17:52:33 +0530

    button change

--- src/components/fixed-plugin.tsx
+++ src/components/fixed-plugin.tsx
@@ -9,6 +9,7 @@ export function FixedPlugin() {
         color="white"
         size="sm"
         className="!fixed bottom-4 right-4 flex gap-1 pl-2 items-center border border-blue-gray-50"
+        placeholder="YourPlaceholderValue"
       >
         <Image
           width={128}
