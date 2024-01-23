a4d21555cd02bcc0bac238fdcf0d2c7501b0388e
MaheshBabu11 <mahesh.b.pec@gmail.com>
2024-01-23 16:16:25 +0530

    video player adjustments

--- src/app/video-intro.tsx
+++ src/app/video-intro.tsx
@@ -7,7 +7,7 @@ import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 export function VideoIntro() {
   return (
     <div className="p-8">
-      <div className="mt-10 mx-10 rounded-xl ">
+      <div className="mt-10 lg:mx-10">
         <LiteYouTubeEmbed id="-UCRipi97Fw" title="Medpaideia Intro" />
       </div>
     </div>
